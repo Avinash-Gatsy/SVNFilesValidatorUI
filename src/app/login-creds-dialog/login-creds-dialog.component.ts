44f83a482143663978d354c4a1e11a6269de6a42
Avinash <avinashg12291992@gmail.com>
2019-04-29 21:04:40 +0530

    initial commit

---
+++ src/app/login-creds-dialog/login-creds-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-creds-dialog',
+  templateUrl: './login-creds-dialog.component.html',
+  styleUrls: ['./login-creds-dialog.component.scss']
+})
+export class LoginCredsDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/path-form/path-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-path-form',
+  templateUrl: './path-form.component.html',
+  styleUrls: ['./path-form.component.scss']
+})
+export class PathFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
